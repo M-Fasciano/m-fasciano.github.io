9bf3e654db3bd54aaff0ac3d4f07383b65f46369
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2020-04-22 13:43:45 +0100

    Self hosted font fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,7 +43,7 @@ module.exports = {
       resolve: 'gatsby-plugin-web-font-loader',
       options: {
         google: {
-          families: 'Major Mono Display',
+          families: ['Major Mono Display'],
         }
       }
     },
