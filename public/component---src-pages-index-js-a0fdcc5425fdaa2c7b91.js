01e3d651f6337f973a7ad8e73a53b3820dce34b0
Michele Fasciano <michelefasciano37@hotmail.co.uk>
2020-05-09 10:08:36 +0100

    Public folder added to gitignore

--- public/component---src-pages-index-js-a0fdcc5425fdaa2c7b91.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{RXBc:function(n,t,u){"use strict";u.r(t);var a=u("q1tI"),e=u.n(a),c=(u("vmCN"),u("yBb5"));u("VAPu");u("7O5W").a.autoAddCss=!1;t.default=function(){return e.a.createElement(c.a,null)}},VAPu:function(n,t,u){}}]);
-//# sourceMappingURL=component---src-pages-index-js-a0fdcc5425fdaa2c7b91.js.map
