587e4e37875f9fccbb3f0df1caf1ad71731cab47
Michele Fasciano <michele.fasciano@collectivelondon.com>
2021-01-28 16:11:52 +0000

    Removed unused code

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -9,40 +9,17 @@ import Header from '../Header/Header'
 import Content from '../Content/Content'
 import Footer from '../Footer/Footer'
 
-
 library.add(fab)
 
-class App extends React.Component {
-
-  // fake authentication Promise
-  authenticate(){
-    return new Promise(resolve => setTimeout(resolve, 2000)) // 2 seconds
-  }
-
-  componentDidMount(){
-    this.authenticate().then(() => {
-      const ele = document.getElementById('loader')
-      if(ele){
-        // fade out
-        ele.classList.add('available')
-        setTimeout(() => {
-          // remove from DOM
-          ele.outerHTML = ''
-        }, 2000)
-      }
-    })
-  }
-
-  render() { 
-    return (
-      <>
-        <SEO />
-        <Header />
-        <Content />
-        <Footer />
-      </>
-    )
-  }
+const App = () => {
+  return (
+    <>
+      <SEO />
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  )
 }
 
 export default App
